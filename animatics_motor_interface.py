031340cc16909baa89464b04d3e4a72b2a4383e4
smpollard0 <spollard301@gmail.com>
2024-01-10 10:25:50 -0800

    add class to create sms programs for animatics motor

--- animatics_motor_interface.py
+++ animatics_motor_interface.py
@@ -5,9 +5,58 @@ animatics_motor_interface.py: this program is suppose to help build the scripts 
 __author__ = "Spencer Pollard"
 __credits__ = ["Spencer Pollard", "Shane Mayor"]
 
+import os
+
 '''
 how I think I should make python interface with the motor
 
 init command to create a temporary script file that will be used to program the motor
 commands that open this temporary file to write appropriate commands to it
 '''
+
+# class to house the program that is to be uploaded to the motor
+class AnimaticsProgram():
+    # constructors
+    def __init__(self):
+        self.file_data = ""
+        self.file_path = ""
+        self.file_name = ""
+    def __init__(self, file_path, file_name):
+        self.file_path = file_path
+        self.file_name = file_name
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    # setters
+        
+    # getters
+
+# 
+def createProgram(file_name):
+
+    try:
+        os.makedirs("./temp")
+    except:
+        pass
+    
+    file = open(f"./temp/{file_name}.sms", "a")
+
+    return file
+
+# 
+def setVelocity(passed_velocity):
+
+    return -1
+
+# 
+def setAcceleration(passed_acceleration):
+
+    return -1
+
+#
+def uploadProgram():
+
+    return -1
+
+if __name__ == "__main__":
+    program = createProgram("bob's burgers")
