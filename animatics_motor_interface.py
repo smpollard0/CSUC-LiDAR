fd344c9dc7795079c584ae03fe4a1e15757be81d
smpollard0 <spollard301@gmail.com>
2024-01-10 10:36:12 -0800

    add constructors

--- animatics_motor_interface.py
+++ animatics_motor_interface.py
@@ -19,29 +19,31 @@ class AnimaticsProgram():
     # constructors
     def __init__(self):
         self.file_data = ""
-        self.file_path = ""
+        self.file_path = "./temp"
         self.file_name = ""
+        os.makedirs(self.file_path)
     def __init__(self, file_path, file_name):
         self.file_path = file_path
         self.file_name = file_name
+        os.makedirs(self.file_path)
+        open(f"./temp/{self.file_name}", "a")
     def __init__(self, file_name):
         self.file_name = file_name
+        os.makedirs(self.file_path)
+        open(f"./temp/{self.file_name}", "a")
 
     # setters
-        
+    def createProgram(self):
+        print("spaghettios")
+
+    def setFileName(self, file_name):
+        self.file_name = file_name
+
+    def setFilePath(self, file_path):
+        self.file_path
+
+
     # getters
-
-# 
-def createProgram(file_name):
-
-    try:
-        os.makedirs("./temp")
-    except:
-        pass
-    
-    file = open(f"./temp/{file_name}.sms", "a")
-
-    return file
 
 # 
 def setVelocity(passed_velocity):
@@ -59,4 +61,4 @@ def uploadProgram():
     return -1
 
 if __name__ == "__main__":
-    program = createProgram("bob's burgers")
+    program = AnimaticsProgram()
