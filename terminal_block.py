1c4a10f1983f4519a26b1e88a17d5cefbac14f5a
smpollard0 <spollard301@gmail.com>
2024-01-11 09:53:48 -0800

    add edge counting task function to attempt to count edges using hte ni pcie-6323

--- terminal_block.py
+++ terminal_block.py
@@ -11,5 +11,24 @@ __credits__ = ["Spencer Pollard", "Shane Mayor"]
 
 # figure out how to use the scc-ft01 cards on the scc-68 with the pcie-6323
 # 
-with ni.Task() as task:
-    print("pain")
+
+def edgeCounting(num_of_samples, passed_sample_rate):
+    sample = num_of_samples
+    sample_rate = passed_sample_rate
+
+    count = 0
+
+    with ni.Task() as task:
+        # create counter input task to count rising edges (default setting) on the Dev2/ctr0 channel
+        task.ci_channels.add_ci_count_edges_chan(counter="Dev2/ctr0")
+        
+        # configure the card's timing to allow for continous data collection 
+        task.timing.cfg_samp_clk_timing(rate=sample_rate, sample_mode=ni.constants.AcquisitionType.CONTINUOUS, samps_per_chan=sample)
+        temp = task.read(number_of_samples_per_channel=sample)
+
+        # place all data from temp into data array
+        
+
+if __name__ == "__main__":
+    edgeCounting(10000,100000000)
+        
