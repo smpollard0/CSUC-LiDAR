f58486bd75c748264d917c6f953e8bb115351d84
smpollard0 <spollard301@gmail.com>
2024-01-11 11:40:14 -0800

    set task to read data for 15 seconds and output final count at the end

--- terminal_block.py
+++ terminal_block.py
@@ -3,14 +3,12 @@ terminal_block.py: This script is attempting to interface the NI DAQ PCIe-6323 M
 """
 
 import nidaqmx as ni
+import time
 
 __author__ = "Spencer Pollard"
 __credits__ = ["Spencer Pollard", "Shane Mayor"]
 
-# keep in mind pylablib for interfacing using the COM serial ports
-
 # figure out how to use the scc-ft01 cards on the scc-68 with the pcie-6323
-# 
 
 def edgeCounting(num_of_samples, passed_sample_rate):
     sample = num_of_samples
@@ -20,13 +18,22 @@ def edgeCounting(num_of_samples, passed_sample_rate):
 
     with ni.Task() as task:
         # create counter input task to count rising edges (default setting) on the Dev2/ctr0 channel
-        task.ci_channels.add_ci_count_edges_chan(counter="Dev2/ctr0")
-        
-        # configure the card's timing to allow for continous data collection 
-        task.timing.cfg_samp_clk_timing(rate=sample_rate, source="/Dev2/PFI8") # encoder A from the motor is screw terminal 37 which is pfi8
-        temp = task.read(number_of_samples_per_channel=ni.constants.READ_ALL_AVAILABLE, timeout=-1) # wait forever until it's acquired the desired number of samples
-        print(temp)
+        task.ci_channels.add_ci_count_edges_chan(counter="/Dev2/ctr0", name_to_assign_to_channel="countEdges")
 
+        result = 0
+
+        # start task
+        task.control(ni.constants.TaskMode.TASK_START)
+
+        t_end = time.time() + 15
+        while time.time() < t_end:
+            result = task.read(number_of_samples_per_channel=ni.constants.READ_ALL_AVAILABLE, timeout=120)[-1]
+            print(result)
+
+        # end task
+        task.control(ni.constants.TaskMode.TASK_STOP)
+        
+        print(f"Final Count: {result}")
 
 
         
