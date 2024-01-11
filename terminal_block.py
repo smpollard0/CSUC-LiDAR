15e7084f825abb6151ca2e1f570b0f9b5af95e19
smpollard0 <spollard301@gmail.com>
2024-01-11 11:43:37 -0800

    remove unused variables

--- terminal_block.py
+++ terminal_block.py
@@ -10,12 +10,7 @@ __credits__ = ["Spencer Pollard", "Shane Mayor"]
 
 # figure out how to use the scc-ft01 cards on the scc-68 with the pcie-6323
 
-def edgeCounting(num_of_samples, passed_sample_rate):
-    sample = num_of_samples
-    sample_rate = passed_sample_rate # for the motor this should be the highest frequency of the pulse train I believe
-
-    count = 0
-
+def edgeCounting():
     with ni.Task() as task:
         # create counter input task to count rising edges (default setting) on the Dev2/ctr0 channel
         task.ci_channels.add_ci_count_edges_chan(counter="/Dev2/ctr0", name_to_assign_to_channel="countEdges")
@@ -33,10 +28,6 @@ def edgeCounting(num_of_samples, passed_sample_rate):
         task.control(ni.constants.TaskMode.TASK_STOP)
         
         print(f"Final Count: {result}")
-
-
-        
-        
 
 if __name__ == "__main__":
     edgeCounting(10000,100000000)
