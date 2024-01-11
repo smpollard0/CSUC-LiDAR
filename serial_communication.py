8087d83979a69cd03ac7dd38c4e88c2e473cc321
smpollard0 <spollard301@gmail.com>
2024-01-11 12:47:43 -0800

    add comments and adjust formatting

--- serial_communication.py
+++ serial_communication.py
@@ -21,11 +21,6 @@ def serial_write(file_path):
         raise Exception("Invalid file path")
 
     temp_in_ascii = temp.encode('ascii')
-    temp_in_hex = temp_in_ascii.hex()
-
-
-    # the above program in hex should be
-    hex_string = "52 55 4E 3F 0A 5A 53 0A 41 3D 31 30 30 0A 56 3D 31 30 30 30 30 30 30 0A 50 3D 31 30 30 30 30 30 0A 47 0A 54 57 41 49 54 0A 50 3D 30 0A 47 0A 50 52 49 4E 54 28 22 50 72 6F 67 72 61 6D 20 46 69 6E 69 73 68 65 64 20 3A 44 22 2C 23 31 33 29 0A 45 4E 44 0A"
 
     # mostly works? currently I believe there is a timing issue where I'm sending bytes before I receieve confirmation of the ones I just sent
     with serial.Serial('COM1', 9600) as ser:
@@ -47,10 +42,7 @@ def serial_write(file_path):
                 remaining_chars -= remaining_chars
             
             ser.write(sub_packet)
-            time.sleep(0.5)
+            time.sleep(0.5) # this is a weird work around to my current issue
             read_bytes = ser.read_all()
             print(read_bytes.decode('utf-8'))
         ser.close()
-
-if __name__ == "__main__":
-    serial_write()
