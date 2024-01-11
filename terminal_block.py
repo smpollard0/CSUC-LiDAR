f76b3508b6b0113471a4d3626d59d73371e6ac85
smpollard0 <spollard301@gmail.com>
2024-01-11 09:38:54 -0800

    rename signal_conditioning_box.py to terminal_block.py

--- terminal_block.py
+++ terminal_block.py
@@ -1,5 +1,5 @@
 """
-signal_conditioning_box.py: This script is attempting to interface the NI DAQ PCIe-6323 Multifunction I/O card with the NI SCC-68
+terminal_block.py: This script is attempting to interface the NI DAQ PCIe-6323 Multifunction I/O card with the NI SCC-68
 """
 
 import nidaqmx as ni
@@ -10,5 +10,6 @@ __credits__ = ["Spencer Pollard", "Shane Mayor"]
 # keep in mind pylablib for interfacing using the COM serial ports
 
 # figure out how to use the scc-ft01 cards on the scc-68 with the pcie-6323
+# 
 with ni.Task() as task:
     print("pain")
